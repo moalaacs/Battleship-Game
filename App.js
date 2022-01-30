18081458ad3182e76b779fe8d2d7cc77b08dda94
Mohammed Alaa <moalaacs@gmail.com>
2022-01-30 22:20:20 +0200

    Implementing the model object

--- App.js
+++ App.js
@@ -13,3 +13,15 @@ var view = {
   },
 };
 
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    { location: ["06", "16", "26"], hits: ["", "", ""] },
+    { location: ["24", "34", "44"], hits: ["", "", ""] },
+    { location: ["10", "11", "12"], hits: ["", "", ""] },
+  ],
+};

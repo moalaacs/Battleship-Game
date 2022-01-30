526d8218975fe6e1065cff64756b1067c6624982
Mohammed Alaa <moalaacs@gmail.com>
2022-01-31 00:33:00 +0200

    Setting up the fire method

--- App.js
+++ App.js
@@ -20,8 +20,21 @@ var model = {
   shipsSunk: 0,
 
   ships: [
-    { location: ["06", "16", "26"], hits: ["", "", ""] },
-    { location: ["24", "34", "44"], hits: ["", "", ""] },
-    { location: ["10", "11", "12"], hits: ["", "", ""] },
+    { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] },
   ],
+
+  fire: function (guess) {
+    for (i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var locations = ship.locations;
+      var index = locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        return true;
+      }
+    }
+    return false;
+  },
 };

1bb2bcc277470d8b78235a7ea185595744c29539
Mohammed Alaa <moalaacs@gmail.com>
2022-02-01 20:36:50 +0200

    Parsing the player's guess

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ var model = {
         view.displayHit(guess);
         view.displayMessage("HIT!");
         if (this.isSunk(ship)) {
-          displayMessage("You sank my battleship!");
+          view.displayMessage("You sank my battleship!");
           this.shipsSunk++;
         }
         return true;
@@ -52,5 +52,32 @@ var model = {
       }
     }
     return true;
+  },
+};
+
+var controller = {
+  parseGuess: function (guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    if (guess === null || guess.length !== 2) {
+      alert("Oops, please enter a letter and number on the board.");
+    } else {
+      var firstChar = guess.charAt(0);
+      var row = alphabet.indexOf(firstChar);
+      var coloum = guess.charAt(1);
+
+      if (isNaN(row) || isNaN(coloum)) {
+        alert("Oops, that isn't on the board.");
+      } else if (
+        row < 0 ||
+        row >= model.boardSize ||
+        coloum < 0 ||
+        coloum >= model.boardSize
+      ) {
+        alert("Oops, that's off the board!");
+      } else {
+        return row + coloum;
+      }
+    }
+    return null;
   },
 };
